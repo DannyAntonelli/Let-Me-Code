7ea600e8b52a315dfd77d05a5c3fcd541cae5490
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-12 18:08:00 +0200

    Change default permission

--- letmecode/letmecode/settings.py
+++ letmecode/letmecode/settings.py
@@ -130,6 +130,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.AllowAny",
     ],
 }
