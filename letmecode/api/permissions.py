c941ebb97abb752bef7a015440846bc63e314dd7
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-12 18:54:02 +0200

    Add IsPublicOrCreatorOrShared permission

--- letmecode/api/permissions.py
+++ letmecode/api/permissions.py
@@ -7,3 +7,15 @@ class IsCreatorOrShared(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user in obj.shared_users.all()
+
+
+class IsPublicOrCreatorOrShared(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.is_public
+            or obj.user == request.user
+            or request.user in obj.shared_users.all()
+        )
