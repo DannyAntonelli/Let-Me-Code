0d40f5724e24e8fa7af2dfec73dc6b15d4a0996f
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-12 19:23:54 +0200

    Remove unused imports

--- letmecode/api/views.py
+++ letmecode/api/views.py
@@ -1,18 +1,15 @@
 from django.db import IntegrityError
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.http import HttpRequest
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 from .models import File, Project
 from .serializers import UserSerializer, FileSerializer, ProjectSerializer
 from .permissions import (
-    IsCreatorOrShared,
     IsPublicOrCreatorOrShared,
     IsFileInPermittedProject,
 )
