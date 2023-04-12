cce412532807ae332e1e50820d4d834cb959e495
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-12 19:17:57 +0200

    Add file permission

--- letmecode/api/permissions.py
+++ letmecode/api/permissions.py
@@ -19,3 +19,13 @@ class IsPublicOrCreatorOrShared(permissions.BasePermission):
             or obj.user == request.user
             or request.user in obj.shared_users.all()
         )
+
+
+class IsFileInPermittedProject(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return IsPublicOrCreatorOrShared.has_object_permission(
+            self, request, view, obj.project
+        )
