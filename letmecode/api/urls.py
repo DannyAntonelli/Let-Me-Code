75a6e9251dae95f088243702a800cf0b57a06f84
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-12 17:49:59 +0200

    Add auth views

--- letmecode/api/urls.py
+++ letmecode/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
 
 
 urlpatterns = [
-    path("", views.index),
+    path("login/", obtain_auth_token),
+    path("register/", views.Register.as_view()),
+    path("test-is-auth/", views.TestIsAuth.as_view()),
 ]
--- letmecode/api/views.py
+++ letmecode/api/views.py
@@ -1,6 +1,44 @@
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+from .models import File, Project
 
 
-def index(request: HttpRequest):
-    return HttpResponse("Hello world!")
+class TestIsAuth(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: HttpRequest):
+        return Response({"message": "You are authenticated"})
+
+
+class Register(APIView):
+    permission_classes = []
+
+    def post(self, request: HttpRequest):
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"message": "Username and password are required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            return Response({"message": "User created successfully"})
+        except IntegrityError:
+            return Response(
+                {"message": "Username already taken"},
+                status=status.HTTP_409_CONFLICT,
+            )
