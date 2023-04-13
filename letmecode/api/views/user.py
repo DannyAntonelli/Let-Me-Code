71aad00a68bf08aa1e255edda89d2145295f5350
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-13 15:56:19 +0200

    Add AllowAny to Register view

--- letmecode/api/views/user.py
+++ letmecode/api/views/user.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,6 +13,8 @@ from ..serializers import UserSerializer
 
 
 class Register(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request: Request) -> Response:
         username = request.data.get("username")
         email = request.data.get("email")
