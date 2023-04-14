84ab9a6d69879f4ad95b7d4dec3bcdf663a9b9ff
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-14 11:42:02 +0200

    Create router folder

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import Profile from './views/Profile.vue'
-import Register from './views/Register.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Profile from '@/views/Profile.vue'
+import Register from '@/views/Register.vue'
 
 
 const routes = [
