f9358ed52198db8f068202264e874a36e29f07c2
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-19 16:50:12 +0200

    Change double quotes to single quotes

--- frontend/src/components/CodeEditor.vue
+++ frontend/src/components/CodeEditor.vue
@@ -1,18 +1,18 @@
 <template>
-    <div id="code-editor" style="height: 500px"></div>
+    <div id='code-editor' style='height: 500px'></div>
 </template>
 
 <script>
-import loader from "@monaco-editor/loader";
+import loader from '@monaco-editor/loader';
 
 export default {
-    name: "CodeEditor",
+    name: 'CodeEditor',
     async mounted() {
         const editorOptions = {
-            language: "python",
+            language: 'python',
         }
         loader.init().then((monaco) => {
-            monaco.editor.create(document.getElementById("code-editor"), editorOptions);
+            monaco.editor.create(document.getElementById('code-editor'), editorOptions);
         });
     }
 }
