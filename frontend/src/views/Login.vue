44a227be717395b8f91abf7c33178519731d0580
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-19 15:53:25 +0200

    Add basic register form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
     <form @submit.prevent="handleLogin">
         <div class="form-outline mb-4">
-            <input type="text" id="form2Example1" class="form-control" v-model="username" required />
-            <label class="form-label" for="form2Example1">Username</label>
+            <input type="text" id="login-username" class="form-control" v-model="username" required />
+            <label class="form-label" for="login-username">Username</label>
         </div>
 
         <div class="form-outline mb-4">
-            <input type="password" id="form2Example2" class="form-control" v-model="password" required />
-            <label class="form-label" for="form2Example2">Password</label>
+            <input type="password" id="login-password" class="form-control" v-model="password" required />
+            <label class="form-label" for="login-password">Password</label>
         </div>
 
         <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,3 +1,55 @@
 <template>
-    <p>Register</p>
+    <form @submit.prevent="handleRegister">
+        <div class="form-outline mb-4">
+            <input type="text" id="register-username" class="form-control" v-model="username" required />
+            <label class="form-label" for="register-username">Username</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="password" id="register-password" class="form-control" v-model="password" required />
+            <label class="form-label" for="register-password">Password</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-block mb-4">Register</button>
+
+        <div class="text-center">
+            <p>Already have an account? <router-link to="/login">Login</router-link></p>
+        </div>
+    </form>
 </template>
+  
+<script>
+import { register, login } from '@/api/user.js'
+
+export default {
+    name: 'Register',
+
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        handleRegister() {
+            register(this.username, this.password)
+                .then(() => {
+                    login(this.username, this.password)
+                        .then(response => {
+                            console.log(response)
+                            localStorage.setItem('token', response.token)
+                            this.$router.push('/')
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+}
+</script>
+  
