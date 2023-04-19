25a71b3fc61864efacd6e953dee28bd0a7d3dbb9
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-19 13:31:17 +0200

    Add basic navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <template>
+  <Navbar />
   <router-view :key="$route.path" />
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar,
+  },
 }
 </script>
 
