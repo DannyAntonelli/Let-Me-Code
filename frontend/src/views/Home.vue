ee1db21464d12b418a069c9eee184e4e242f3cb7
DannyAntonelli <dannyantonelli2002@gmail.com>
2023-04-24 12:46:40 +0200

    Add basic home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,57 @@
 <template>
-  <p>Home</p>
+  <h1 class="text-center p-5">
+    <font-awesome-icon icon="fa-solid fa-terminal" beat-fade />
+    <strong> Code, Share, Collaborate</strong>
+  </h1>
+
+  <div class="container text-center text-md-start">
+    <div class="row mt-3 text-center">
+      <div class="col mx-auto mb-4">
+        <p>
+          <strong> Let Me Code</strong> is a platform for developers to share
+          their code, collaborate with other developers, and learn from each
+          other. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quisquam voluptates, quod, quia, voluptate quae voluptatem quibusdam
+          quos voluptatum quas quidem nesciunt. Quisquam, quae. Quisquam, quae.
+        </p>
+      </div>
+
+      <div class="col mx-auto mb-4">
+        <h5><strong>Join Our Community:</strong></h5>
+        <router-link to="/register"
+          ><button class="btn btn-primary btn-lg">
+            Start for free
+          </button></router-link
+        >
+      </div>
+    </div>
+  </div>
+
+  <div class="container text-center text-md-start mt-5 pt-2">
+    <div class="row mt-3 text-center">
+      <div class="col mx-auto mb-4">
+        <h4><strong>Zero Setup</strong></h4>
+        <p>
+          Start working on your code instantly, without having any code editor
+          or IDE installed on your machine.
+        </p>
+      </div>
+
+      <div class="col mx-auto mb-4">
+        <h4><strong>Zero Setup</strong></h4>
+        <p>
+          Start working on your code instantly, without having any code editor
+          or IDE installed on your machine.
+        </p>
+      </div>
+
+      <div class="col mx-auto mb-4">
+        <h4><strong>Zero Setup</strong></h4>
+        <p>
+          Start working on your code instantly, without having any code editor
+          or IDE installed on your machine.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
