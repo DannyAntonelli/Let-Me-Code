5a9be5c502b4048bcb222cdbacf9fee4af0ac15c
vmcvastry <009valerio@gmail.com>
2023-04-28 17:59:42 +0200

    Fix file paths

--- frontend/src/components/workspace/SideBar.vue
+++ frontend/src/components/workspace/SideBar.vue
@@ -18,15 +18,19 @@
   function buildTree(files) {
     let tree = {};
     for (let file of files) {
-      let path = ("/" + file.name).split("/");
+      let path = file.name.split("/");
       let curr = tree;
       for (let i = 0; i < path.length; i++) {
         if (i == path.length - 1) {
-          curr[path[i]] = file.name;
+          file.fileName = path[i];
+          curr[path[i]] = file;
         } else {
           if (curr[path[i]] == undefined) {
-            // let k = path[i] != "" ? path[i] : "root";
-            curr[path[i]] = {};
+            // console.log("f", path.slice(1, i + 1));
+
+            curr[path[i]] = {
+              folderName: path.slice(0, i + 1).join("/") + "/",
+            };
           }
           curr = curr[path[i]];
         }
@@ -68,18 +72,18 @@
       //   this.files.push(f.file);
       // }
       this.files = [
-        { id: 1, name: "file1", content: "", language: "py", project: 1 },
-        { id: 2, name: "file3", content: "", language: "py", project: 1 },
+        { id: 1, name: "/file1", content: "", language: "py", project: 1 },
+        { id: 2, name: "/file3", content: "", language: "py", project: 1 },
         {
           id: 3,
-          name: "folder/file2",
+          name: "/folder/file2",
           content: "",
           language: "py",
           project: 1,
         },
         {
           id: 4,
-          name: "folder/file5",
+          name: "/folder/file5",
           content: "",
           language: "py",
           project: 1,
