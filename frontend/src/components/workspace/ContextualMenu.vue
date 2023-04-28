d7cd0a174fb9ab90ad1bc344771f2ebfa9500948
vmcvastry <009valerio@gmail.com>
2023-04-28 16:14:07 +0200

    Contextual menu

---
+++ frontend/src/components/workspace/ContextualMenu.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="context-menu"
+    v-show="visible"
+    :style="{ top: position.y + 'px', left: position.x + 'px' }"
+  >
+    <ul>
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        @click="handleItemClick(item.action)"
+      >
+        {{ item.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ContextMenu",
+
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      },
+      position: {
+        type: Object,
+        required: true,
+      },
+      visible: {
+        type: Boolean,
+        required: true,
+      },
+      param: {
+        type: Object,
+      },
+    },
+    methods: {
+      handleItemClick(action) {
+        if (typeof action === "function") {
+          action(this.param);
+        }
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .context-menu {
+    position: absolute;
+    z-index: 100;
+    background-color: white;
+    border: 1px solid #ccc;
+  }
+
+  .context-menu ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .context-menu li {
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+
+  .context-menu li:hover {
+    background-color: #eee;
+  }
+</style>
