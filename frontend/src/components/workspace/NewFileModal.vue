037787335b42a2b7db0814345b10eef885dadf0d
vmcvastry <009valerio@gmail.com>
2023-04-28 18:00:06 +0200

    New file modal

---
+++ frontend/src/components/workspace/NewFileModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <!-- <button
+    type="button"
+    class="btn btn-success"
+    data-bs-toggle="modal"
+    data-bs-target="#newFileModal"
+  >
+    New File
+  </button> -->
+
+  <div
+    class="modal fade"
+    id="newFileModal"
+    tabindex="-1"
+    aria-labelledby="newFileModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="newFileModalLabel">New File</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <input
+                type="text"
+                class="form-control"
+                id="new-file-name"
+                v-model="newName"
+              />
+            </div>
+
+            <div class="mb-3">
+              <input
+                type="text"
+                class="form-control"
+                id="new-file-language"
+                placeholder="Language"
+                v-model="lang"
+              />
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            id="dismiss-modal"
+          >
+            Cancel
+          </button>
+          <button
+            type="button"
+            class="btn btn-success"
+            data-dismiss="modal"
+            @click="submitFile"
+          >
+            Create file
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  //   import { Modal } from "bootstrap";
+
+  export default {
+    name: "NewFileModal",
+
+    data() {
+      return {
+        language: "",
+        newName: "",
+      };
+    },
+    props: {
+      path: String,
+    },
+    methods: {
+      submitFile() {
+        // this.$emit("submit-project", {
+        //   name: this.name,
+        //   description: this.description,
+        //   isPublic: this.isPublic,
+        // });
+        // document.getElementById("dismiss-modal").click();
+        // this.name = "";
+        // this.description = "";
+        // this.isPublic = true;
+        console.log(this.path, this.language);
+      },
+    },
+    async mounted() {
+      console.log(this.path, "aaaaaaaaa");
+      this.newName = this.path;
+      //   let m = new Modal(document.querySelector("#newFileModal"));
+      //   m.show();
+    },
+  };
+</script>
